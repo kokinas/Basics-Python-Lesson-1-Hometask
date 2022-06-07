f5c0cacae1675a0bb53fddeea07c9ca3aacb7c85
Kokin Anton <anton44192@gmail.com>
2022-06-07 17:16:59 +0500

    Lesson 1.1 and Leson 1.2 complete"

---
+++ Lesson_1.py
@@ -0,0 +1,19 @@
+"""
+# Lesson #1.1 play with varribles:
+print('Lesson #1.1 play with varribles:')
+a = input ('tape some first numb: ')
+b = input ('tape some second numb: ')
+c =int(a)**int(b)
+print(f'first numb is {a}, second numb is {b}, a^b = {c}')
+print('-'*100)
+"""
+"""
+# Lesson #1.2 seconds convert to format hh:mm:ss:
+print('Lesson #1.2 seconds convert to format hh:mm:ss ')
+val_sec = int(input('tape value seconds and I will convert it to format hh:mm:ss '))
+hours = val_sec // 3600
+minits = (val_sec % 3600) // 60
+seconds = val_sec % 60
+print('Do not thanks leather bag: %02i:%02i:%02i' %(hours, minits, seconds))
+"""
+
