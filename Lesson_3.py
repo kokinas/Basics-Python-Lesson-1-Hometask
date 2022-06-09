1d0e7a79c76dfbbd76f2f35c1767781358e7197c
kokinas <anton44192@gmail.com>
2022-06-09 22:48:21 +0500

    Tasks 3.1 - 3.4 are complete

--- Lesson_3.py
+++ Lesson_3.py
@@ -1,6 +1,6 @@
 #Home task Basics python
 ###Lesson #3 FUNCTION
-
+'''
 #Task #3.1 divide function
 def divide(divident, divisor):
 	"""
@@ -18,6 +18,82 @@ def divide(divident, divisor):
 print(divide(10, 0))
 print("that's all")
 
+
 #Task #3.2 print user data
-def print_user_data(name, surname, birthday, town, e-mail, tele_num):
+def print_user_data(name, surname, birthday, town, email, tele_num):
+	"""
+	make sentense with users data
+
+	(string, string, string, string, string, string, string) -> string
+
+	>>> print_user_data('Ivan', 'Ivanov', '01.01.1990', 'Moscow', 'ivanovII@gmail.com', '+79999879898')
+
+	"Ivan Ivanov 01.01.1990 Moscow ivanovII@gmail.com +79999879898"
+
+	"""
+	return('{} {} {} {} {} {}'.format(name, surname, birthday, town, email, tele_num))
+
+print (print_user_data('Ivan', 'Ivanov', '01.01.1990', 'Moscow', 'ivanovII@gmail.com', '+79999879898'))
+
+
+#Task #3.3 sum two maximum
+
+def sum_max(numb1, numb2, numb3):
+	"""
+	return sum of two maximum number
+
+	(number, number, number) -> number
+
+	>>> sum_max(3, 8, 4)
+	12
+	"""
+	list1 = [numb1, numb2, numb3]
+	list1.remove(min(list1))
+	return sum(list1)
+
+print (sum_max(3, 8, 3))
+
+
+#Task #3.4.1 exponentiation
+def exp1(x, y):
+	"""
+	#return x exponent negative y
+
+	(number, number) -> number
 	
+	exp(4, -4)
+	256
+	"""
+
+	if y < 0:
+		y = (-1) * y
+	else:
+		return "'y' must be positive number"
+	return (x**y)
+
+print (exp1(4, -4))
+
+#Task #3.4.2 exponentiation
+def exp2(x, y):
+	"""
+	#return x exponent negative y
+
+	(number, number) -> number
+	
+	exp(4, -4)
+	256
+	"""
+
+	result = x
+	if y < 0:
+		y = (-1) * y
+	else:
+		return "'y' must be positive number"
+	for i in range(y - 1):
+		result = result * x
+	return result
+
+print (exp2(4, -4))
+'''
+
+#Task #3.5
