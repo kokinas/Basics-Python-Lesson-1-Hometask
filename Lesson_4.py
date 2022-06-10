ed0eb8bf39a1c38008b19f76afd95fa9522cd8d0
Kokin Anton <anton44192@gmail.com>
2022-06-10 17:20:39 +0500

    Tasks 4.1, 4.3, 4.4 are completed

--- Lesson_4.py
+++ Lesson_4.py
@@ -1,16 +1,22 @@
+# Task 4.1 wage script
+"""
+import sys
+n, hour, wage, prem = sys.argv
+print (f'total wage: {int(hour) * int(wage) + int(prem)}$')
+"""
+
+#Task 4.2 transform some list
+x = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+print([y for y in x if y < x [x.index(y) + 1]])
+
+"""
+# Task 4.3 multiple of 20, 21
+print([y for y in range(20, 240) if (y % 20 == 0) or (y % 21 == 0)])
 
 
-#Task #4.1 function salary of employee
-def calc_wage(hour, wage, prem):
-	"""
-	calculete total wage of employee
+# Task 4.4 without repeted number
+x = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+print([i for i in x if x.count(i) == 1])
+"""
 
-	(number, number, number) -> number
-
-	>>> calc_wage(1000, 5, 200)
-	5200
-	"""
-	return hour * wage + prem
-
-print(calc_wage(1000, 5, 200))
-print(calc_wage(924, 633, 22100))
+# Task 4.5 
